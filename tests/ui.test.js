2061dad8039a015e37db6b17038240bca27df2a2
ZhivkoTringov <zhivkotringov@gmail.com>
2023-11-30 22:50:09 +0200

    Test: User Email is visible

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -45,3 +45,13 @@ test('Verify "Add Book" link is visible after user login', async ({ page}) => {
     const isLinkVisible = await AddBooksLink.isVisible();
     expect(isLinkVisible).toBe(true);
 });
+
+test('Verify "User email" is visible after user login', async ({ page}) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[id="password"]', '123456');
+    await page.click('input[type="submit"]');
+    const User_email = await page.$('#user>span');
+    const isLinkVisible = await User_email.isVisible();
+    expect(isLinkVisible).toBe(true);
+});
