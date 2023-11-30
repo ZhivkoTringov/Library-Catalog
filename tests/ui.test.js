20718027e2a6a98cd96f669ef9d7b439f0f62c7c
ZhivkoTringov <zhivkotringov@gmail.com>
2023-11-30 20:04:41 +0200

    Add test link is visible after user login

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -8,10 +8,20 @@ test('Verify "All Books" link is visible', async ({ page}) => {
     expect(isLinkVisible).toBe(true);
 });
 
-test('Verify "Login Button" link is visible', async ({ page}) => {
+test('Verify "Login Button" is visible', async ({ page}) => {
     await page.goto('http://localhost:3000');
     await page.waitForSelector('nav.navbar');
     const loginButton = await page.$('a[href="/login"]');
     const isLoginButtonVisible = await loginButton.isVisible();
     expect(isLoginButtonVisible).toBe(true);
+});
+
+test('Verify "All Books" link is visible after user login', async ({ page}) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[id="password"]', '123456');
+    await page.click('input[type="submit"]');
+    const allBooksLink = await page.$('a[href="/catalog"]');
+    const isLinkVisible = await allBooksLink.isVisible();
+    expect(isLinkVisible).toBe(true);
 });
